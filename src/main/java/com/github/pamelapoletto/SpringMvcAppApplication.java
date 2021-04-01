6cfcadf6331fc3ac04b7e912ee27247e6b990a18
pameladilly <pameladilly@hotmail.com>
2021-04-01 06:18:02 -0300

    Commit inicial

---
+++ src/main/java/com/github/pamelapoletto/SpringMvcAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.pamelapoletto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/github/pamelapoletto/controllers/IndexController.java
@@ -0,0 +1,4 @@
+package com.github.pamelapoletto.controllers;
+
+public class IndexController {
+}
